b1a65430d896e2bb53acdec7bf6ad2f34fec46a3
TianYuZhang <acoupworking@gmail.com>
2022-06-06 01:31:49 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+        'project': './tsconfig.json',
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint',
+        'react-hooks',
+        'prettier'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'prettier/prettier': 'error',
+    }
+};
